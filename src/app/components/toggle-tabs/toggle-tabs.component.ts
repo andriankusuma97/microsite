7361b8b6936e71d389f7f936371f3a4c951e6a74
Syamsul Bahri <Syamsul.Bahri@bni.co.id>
2023-12-11 10:29:58 +0700

    test toggle

--- src/app/components/toggle-tabs/toggle-tabs.component.ts
+++ src/app/components/toggle-tabs/toggle-tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toggle-tabs',
@@ -10,10 +11,17 @@ export class ToggleTabsComponent {
   // dataTabs =['Pemasukan','Pengeluaran' ]
   @Input() dataTabs: string[] = [];
   isActive = 'Pemasukan'
+  constructor(private router: Router) {}
 
+  navigateToNextPage() {
+    this.router.navigate(['/test']); // Navigasi ke '/next' (sesuaikan dengan path yang sesuai)
+  }
 
   onClick(item:string){
     console.log("<<<<<< masuk ",item)
+    this.router.navigate(['/test']); // Navigasi ke '/next' (sesuaikan dengan path yang sesuai)
+
     this.isActive=item
+    
   }
 }
